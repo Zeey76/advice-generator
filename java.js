409c5258459ac4235979dbac257aae04143a38eb
Zeey76 <abdulrasaqzainab76@gmail.com>
2024-12-17 22:01:22 +0100

    Update java.js

--- java.js
+++ java.js
@@ -5,10 +5,19 @@ const button = document.querySelector(".dice");
 
 async function fetchAdvice() {
     try {
+        //Show "Loading..." while fetching
+        advice.textContent = "Loading...";
+        heading.style.display = "none";
+
+        //Fetch the advice
         const response = await fetch("https://api.adviceslip.com/advice");
         if (!response.ok) throw new Error('Failed to fetch advice');
 
+        //Parse the advice
         const data = await response.json();
+
+        //Display the advice after fetching
+        heading.style.display = "block";
         id.textContent = data.slip.id;
         advice.textContent = data.slip.advice;
     } catch (error) {
